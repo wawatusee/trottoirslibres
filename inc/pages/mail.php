3b32a8384d77d1af4193563b38648081b6ae5931
wawatusee <web2020.kieran.labarrere@gmail.com>
2024-05-24 16:35:50 +0200

    mail.php, index.php, main.css, refs.json, forme mail, passage du formulaire à l'apperçu du mail, edition du mail, début mise en forme apperçu.

--- inc/pages/mail.php
+++ inc/pages/mail.php
@@ -1,7 +1,7 @@
 
             <!--données inhérentes au mail-->
             <?php $mailModelContents=$LexiqueView->getSectionLexique("mail"); 
-        var_dump($mailModelContents)?>
+       // var_dump($mailModelContents)?>
         <?php $subject=$mailModelContents->subject[$lang]?>
         <?php $Salutation=$mailModelContents->body[$lang]['salutation']?>
         <?php $introduction=$mailModelContents->body[$lang]['introduction']?>
@@ -9,22 +9,23 @@
     <!-- Aperçu du mail -->
     <div id="mailPreviewContainer" class="hidden">
     <!--données inhérentes au mail-->
-        <h2>Aperçu du Mail</h2>
+        <h2>Aperçu du Mail :</h2>
         <!--Sujet-->
-        <h2><?=$subject?></h2>
-        <p><?=$Salutation?></p>
-        <p><?=$introduction?></p>
-        <p><?=$raison?><span id="mailAddress"></span><span id="mailNumber"></span><span id="mailPostCode"></span><span id="mailMunicipality"></span></p>
-        <p>Adresses : </p>
-        <p><strong>Numéro :</strong> </p>
-        <p><strong>Code Postal :</strong> </p>
-        <p><strong>Commune :</strong> </p>
-        <p><strong>Type d'encombrement :</strong> <span id="mailTypeEncombrement"></span></p>
+        <div class="mailPreview">
+            <h2><?=$subject?></h2>
+            <p><?=$Salutation?></p>
+            <p><?=$introduction?><span id="mailTypeEncombrement"></span></p>
+            <p><?=$raison?><span id="mailNumber"></span><?=" "?><span id="mailAddress"></span><?=", "?><span id="mailPostCode"></span><?=" "?><span id="mailMunicipality"></span></p>
+            
+
+            <button id="sendMailButton">Envoyer le Mail</button>
+            <button id="editFormButton">Modifier</button>
         <p><strong>Nom :</strong> <span id="mailName"></span></p>
         <p><strong>Prénom :</strong> <span id="mailFirstName"></span></p>
         <p><strong>Email :</strong> <span id="mailEmail"></span></p>
-        <button id="sendMailButton">Envoyer le Mail</button>
-        <button id="editFormButton">Modifier</button>
+
+        </div>
+
     </div>
     <script>
     document.getElementById('editFormButton').addEventListener('click', () => {
